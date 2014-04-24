df8114a10dbff64f0e80f7790a976b0a18588eb5
Avenger7x <avenger7x13@gmail.com>
2014-04-24 12:43:16 +0300

    flags

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your awesome code here
+function hasFlags(args, flags){
+	return flags.every(args.indexOf.bind(args));
+}
+
+function identity(value){
+	return value;
+}
+
+module.exports = function(fn, commands, parseArgs){
+	var argv = process.argv;
+
+	parseArgs = parseArgs || identity;
+
+	if(hasFlags(argv, ['-h', '--help'])){
+		return console.log(commands.help);
+	}
+
+	if(hasFlags(argv, ['-v', '--version'])){
+		return console.log(commands.version);
+	}
+
+	console.log(fn.apply(null, parseArgs(argv)));
+};
